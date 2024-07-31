6c29a5a183bd7651774a603744080a4c63d1ea46
zhangxiran <569752910@qq.com>
2024-07-31 16:19:07 +0800

    change bootrom for verification

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ cp -r riscv-vector/src/main/scala/* src/main/scala/smartvector/.
 find src/main/scala/smartvector/. -name "*.scala" |xargs -n1 sed -i 's/chipsalliance\.rocketchip\.config/org\.chipsalliance\.cde\.config/g'
 find src/main/scala/smartvector/. -name "*.scala" |xargs -n1 sed -i 's/freechips\.rocketchip\.config/org\.chipsalliance\.cde\.config/g'
 sudo chmod -R 777 ./src/main/scala/smartvector
-sed -i '/object FCMA extends App {/,/^}/d' ./src/main/scala/smartvector/darecreek/exu/fu/fp/fudian/FCMA.scala
-sed -i '/object FCMA extends App {/,/^}/d' ./src/main/scala/smartvector/darecreek/exu/vfu/fp/fudian/FCMA.scala
+#sed -i '/object FCMA extends App {/,/^}/d' ./src/main/scala/smartvector/darecreek/exu/fu/fp/fudian/FCMA.scala
+#sed -i '/object FCMA extends App {/,/^}/d' ./src/main/scala/smartvector/darecreek/exu/vfu/fp/fudian/FCMA.scala
 sed -i '/class HellaCacheExceptions extends Bundle {/,/}/d' ./src/main/scala/smartvector/smartVector/RocketIntf.scala
 # 在下面两个文件中
 # ./src/main/scala/smartvector/darecreek/exu/fu/fp/fudian/FCMA.scala
--- src/main/scala/devices/tilelink/BootROM.scala
+++ src/main/scala/devices/tilelink/BootROM.scala
@@ -14,10 +14,13 @@ import java.nio.ByteBuffer
 import java.nio.file.{Files, Paths}
 
 /** Size, location and contents of the boot rom. */
+object BootROMConfig {
+  var UVM_DEBUG = true
+}
 case class BootROMParams(
   address: BigInt = 0x10000,
   size: Int = 0x10000,
-  hang: BigInt = 0x10040, // The hang parameter is used as the power-on reset vector
+  hang: BigInt = if(BootROMConfig.UVM_DEBUG)0x10000 else 0x10040, // The hang parameter is used as the power-on reset vector
   contentFileName: String)
 
 class TLROM(val base: BigInt, val size: Int, contentsDelayed: => Seq[Byte], executable: Boolean = true, beatBytes: Int = 4,
